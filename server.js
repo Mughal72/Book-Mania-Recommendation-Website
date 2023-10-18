1c5eeada1f6bc57f23ae66965f67cb0279484a2f
Anber sattar <82819709+Mughal72@users.noreply.github.com>
2023-10-18 12:51:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Define your routes or integrate with Flask API
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Book Mania app!');
+});
+
+// Add more route handlers or integrate with Flask API as needed
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
